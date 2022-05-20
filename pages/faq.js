dd993e4a30faa37a4a0e01c9f09ae1127e0e97a9
Dylan Goings <dylan@dgoings.com>
2022-05-20 12:47:32 -0400

    Updating faq

--- pages/faq.js
+++ pages/faq.js
@@ -16,8 +16,105 @@ export default function Faq() {
           Frequently Asked Questions
         </p>
 
-        <h3>Coming Soon...</h3>
-        <p>Please hang tight</p>
+        <ul className="faqs">
+          <li>
+            <p className="question">Where is the wedding taking place?</p>
+            <p className="answer">At the Ypsilanti Freighthouse, 100 Market Pl, Ypsilanti, MI 48198</p>
+          </li>
+          <li>
+            <p className="question">What time is the ceremony? What time is the reception?</p>
+            <p className="answer">We’re starting the celebration with dinner! There will be a short cocktail “hour” beginning at 5:15 with drinks and appetizers. Please plan to be seated at your dinner table no later than 6:00pm. Dinner will be followed directly by the ceremony. Then we’ll transition to dessert, coffee, drinks and dancing! Please refer to this [timeline] if needed.</p>
+          </li>
+          <li>
+            <p className="question">What is the dress code? What should I wear? Is there a theme?</p>
+            <p className="answer">We don’t have a theme or official wedding colors or anything like that. Attire is semi-formal. Dylan and Elizabeth value fun and creativity over convention. Vintage, non-traditional, very traditional, gender-queer outfits are all encouraged. The more memorable you can make this for us, the better!</p>
+          </li>
+          <li>
+            <p className="question">What weather should I expect?</p>
+            <p className="answer">Of course, we can’t guarantee anything, but June can sometimes be hot and humid in Michigan. Plan for warm weather but be prepared for it to cool off later in the evening.</p>
+          </li>
+          <li>
+            <p className="question">Will the wedding be indoors or outdoors?</p>
+            <p className="answer">The wedding and reception will be indoors. As a result, proof of vaccination will be required for all. That said, we are making sure that the event will have excellent ventilation, to ensure everyone’s safety. You can read more about it the FAQ below regarding COVID health and safety protocols.</p>
+          </li>
+          <li>
+            <p className="question">Will transportation be provided? Is there easy parking at the venue(s)?</p>
+            <p className="answer">We are not providing any formal transportation for the event, unless you are staying at our designated hotel for which there will be a shuttle.
+            <br />Our ceremony and reception are located in the same space, so there won’t be any transit between them. There is not private parking at our event location, but there is ample, free public parking for those who feel comfortable driving, and we’ll provide parking maps closer to the event. Lyft/Uber is a great option for those who don’t want to drive themselves. Please stay safe and default to a ride service or carpooling if you plan to drink.</p>
+          </li>
+          <li>
+            <p className="question">Are there other wedding events I can attend?</p>
+            <p className="answer">Yes! Our hope is to create opportunities to catch up with our guests outside of the wedding reception itself, so we have organized three different events open to all guests. No RSVP necessary, however letting us know whether you are planning to attend would be greatly appreciated.
+            <br /><b>Thursday, June 23rd, 7:30-930pm:</b> There will be an informal gathering with desserts and drinks hosted at Dylan’s mom’s house.
+            <br /><b>Saturday, June 25th:</b> Dylan and Elizabeth will host a brunch consisting of bagels and coffee at their house for all guests starting at 11am.
+            <br /><b>Saturday evening:</b> We are planning to attend the <a href="https://a2sf.org/">A2 Summer Festival</a> at the University of Michigan, which is free and open to the public. Our plan is to show up at around 6pm, with some chairs and blankets. This is not an exclusive event for our wedding, just an opportunity for additional socializing with all our friends and family who’d like to join us for some live music!
+            <ul>
+              <li><a href="https://a2sf.org/events/canopy-by-pneuhaus-and-bike-powered-events/">Canopy by Pinehouse, 6pm</a></li>
+              <li><a href="https://a2sf.org/events/ladama/">LATAMA Concert, 8:30pm</a></li>
+            </ul>
+            Closer to the date we will provide more information about getting to the A2 Summer Festival.</p>
+          </li>
+          <li>
+            <p className="question">How do I RSVP?</p>
+            <p className="answer">Please <a href="https://barrioings.online-rsvp.com/">follow this link to RSVP</a>, or just communicate your intentions directly to Dylan via email, text, or phone call.</p>
+          </li>
+          <li>
+            <p className="question">When is the RSVP deadline?</p>
+            <p className="answer">Please get us your RSVP by June 3rd, so we have time to buy enough booze.</p>
+          </li>
+          <li>
+            <p className="question">Can I bring my kids? Will there be a babysitter?</p>
+            <p className="answer">Kids are absolutely invited. We won’t have any childcare provided, and please note that we are not expecting the DJ to keep the music kid-friendly during the party.</p>
+          </li>
+          <li>
+            <p className="question">Can I bring my partner/spouse/romantic other?</p>
+            <p className="answer">Yes please! Just make sure we know how many people you are RSVPing.</p>
+          </li>
+          <li>
+            <p className="question">I can’t attend the wedding in person. Will there be a virtual wedding celebration?</p>
+            <p className="answer">We won’t be streaming or otherwise broadcasting our wedding virtually. Our intention with this event was to bring together friends and family in person. Amidst this ongoing pandemic, the chance to be present with you all is a gift that we don’t want to distract from. For those of you unable to attend, we can’t wait for the next opportunity to see you in person!</p>
+          </li>
+          <li>
+            <p className="question">Do you have a hotel block for guests? Where do you recommend I stay?</p>
+            <p className="answer">Yes, please <a href="https://www.marriott.com/events/start.mi?id=1646233047781&key=GRP">follow this link</a>!</p>
+          </li>
+          <li>
+            <p className="question">Are the ceremony and reception location wheelchair accessible?</p>
+            <p className="answer">Yes!</p>
+          </li>
+          <li>
+            <p className="question">What type of food and drink will be served during the cocktail hour and reception?</p>
+            <p className="answer">We’ll be serving light snacks during cocktail hour, followed by a delicious dinner, and of course dessert and coffee. There will be an open bar for the duration of the event.</p>
+          </li>
+          <li>
+            <p className="question"></p>
+            <p className="answer"></p>
+          </li>
+          <li>
+            <p className="question"></p>
+            <p className="answer"></p>
+          </li>
+          <li>
+            <p className="question"></p>
+            <p className="answer"></p>
+          </li>
+          <li>
+            <p className="question"></p>
+            <p className="answer"></p>
+          </li>
+          <li>
+            <p className="question"></p>
+            <p className="answer"></p>
+          </li>
+          <li>
+            <p className="question"></p>
+            <p className="answer"></p>
+          </li>
+          <li>
+            <p className="question"></p>
+            <p className="answer"></p>
+          </li>
+        </ul>
       </main>
 
       <style jsx>{`
@@ -37,11 +134,6 @@ export default function Faq() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-
-        a {
-          // color: inherit;
-          // text-decoration: none;
         }
 
         .title a {
@@ -71,57 +163,30 @@ export default function Faq() {
           font-size: 1.5rem;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono,
-            Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
+        .faqs {
           margin: 1rem;
-          // flex-basis: 45%;
           padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
+          list-style-type: none;
+          max-width: 600px;
         }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+        .faqs li {
+          margin: 1rem 0;
         }
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
+        .faqs li p {
           margin: 0;
-          font-size: 1.25rem;
           line-height: 1.5;
         }
 
-        .logo {
-          height: 1em;
+        .faqs li p.question {
+          font-size: 1.5rem;
+          font-weight: bold;
+        }
+
+        .faqs li p.answer {
+          margin-top: 0.5rem;
+          font-size: 1.2rem;
         }
 
         @media (max-width: 600px) {
